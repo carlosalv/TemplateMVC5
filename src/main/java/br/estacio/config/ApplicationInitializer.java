6947ae8b7dfa205379d8061c70eaf491e5090080
Carlos Alberto <allbertcarlo@gmail.com>
2017-08-13 18:05:33 -0300

    Projeto Alterado com sucesso

---
+++ src/main/java/br/estacio/config/ApplicationInitializer.java
@@ -0,0 +1,28 @@
+package br.estacio.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses(){
+		return new Class[]{	WebConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[]{"/"};
+	}
+	
+	
+}
